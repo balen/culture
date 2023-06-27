91b45599a3d56c0b84291a6212131f9990336a74
Henry Balen <henry.balen@gmail.com>
2023-06-27 11:37:21 -0400

    Fix score calculator

--- app/services/score_calculator.rb
+++ app/services/score_calculator.rb
@@ -2,32 +2,32 @@ class ScoreCalculator
   # These weights are derived from the sample spreadsheet
   WEIGHTS = {
     PS: {
-      PS01: { invert: true, weight: 2.0},
-      PS02: { invert: false, weight: 2.0},
-      PS03: { invert: true, weight: 2.0},
-      PS04: { invert: false, weight: 2.0},
-      PS05: { invert: true, weight: 2.0},
-      PS06: { invert: false, weight: 2.0},
-      PS07: { invert: false, weight: 2.0}
+      PS01: { invert: true, weight: Rational(100, 49)},
+      PS02: { invert: false, weight: Rational(100, 49)},
+      PS03: { invert: true, weight: Rational(100, 49)},
+      PS04: { invert: false, weight: Rational(100, 49)},
+      PS05: { invert: true, weight: Rational(100, 49)},
+      PS06: { invert: false, weight: Rational(100, 49)},
+      PS07: { invert: false, weight: Rational(100, 49)}
     },
     TM: {
+      # Goes from -100 to +100
       TM01: { invert: false, weight: 10.0},
       TM02: { invert: false, weight: 5.0},
-      TM03: { invert: false, weight: 1.66},
-      TM04: { invert: false, weight: -1.66},
+      TM03: { invert: false, weight: Rational(10, 6)},
+      TM04: { invert: false, weight: Rational(-10, 6)},
       TM05: { invert: false, weight: -5.0},
       TM06: { invert: false, weight: -10.0}
     },
     GM: {
-      GM01: { invert: true, weight: 1.8},
-      GM02: { invert: true, weight: 1.8},
-      GM03: { invert: true, weight: 1.8},
-      GM04: { invert: true, weight: 1.8},
-      GM05: { invert: false, weight: 1.8},
-      GM06: { invert: false, weight: 1.8},
-      GM07: { invert: false, weight: 1.8},
-      GM08: { invert: false, weight: 1.8}
-      
+      GM01: { invert: true, weight: Rational(100, 56)},
+      GM02: { invert: true, weight: Rational(100, 56)},
+      GM03: { invert: true, weight: Rational(100, 56)},
+      GM04: { invert: true, weight: Rational(100, 56)},
+      GM05: { invert: false, weight: Rational(100, 56)},
+      GM06: { invert: false, weight: Rational(100, 56)},
+      GM07: { invert: false, weight: Rational(100, 56)},
+      GM08: { invert: false, weight: Rational(100, 56)}
     }
   }
 
@@ -35,8 +35,8 @@ class ScoreCalculator
   # calc.psychological_safety(organization_id: "47b8292c-3266-483a-b5fb-bb21d239d6c4", access_code: "ABCD")
 
   # we get the avareage for each question - based on the number of responses
-  def summarize(scores:)
-    scores.map{|s, v| v }.reduce(:+) / scores.count
+  def total(scores:)
+    scores.map{|s, v| v }.reduce(:+).to_f
   end
 
   def psychological_safety(organization_id:, access_code:)
@@ -75,21 +75,27 @@ class ScoreCalculator
     max
   end
 
+  # need dimension of the question
+  # s = calc.psychological_safety(organization_id: "47b8292c-3266-483a-b5fb-bb21d239d6c4", access_code: "ABCD")
   def individual_scores(organization_id:, access_code:, group_short_code:)
     scores = {}
+    counts = {}
     response_data = View::ResponseView.responses(organization_id: organization_id, access_code: access_code, group_short_code: group_short_code)
 
     response_data.each do |submission_id, responses|
       responses.each do |response|
-        scores[submission_id] ||= 0
+        scores[response.short_code] ||= 0
+        counts[response.short_code] ||= 0
         if WEIGHTS[group_short_code][response.short_code.to_sym][:invert]
-          scores[submission_id] += (8 - response.response_as_text.to_i) * WEIGHTS[group_short_code][response.short_code.to_sym][:weight]
+          scores[response.short_code] += (8 - response.response_as_text.to_i) * WEIGHTS[group_short_code][response.short_code.to_sym][:weight]
         else
-          scores[submission_id] += (8 - response.response_as_text.to_i) * WEIGHTS[group_short_code][response.short_code.to_sym][:weight]
+          scores[response.short_code] += response.response_as_text.to_i * WEIGHTS[group_short_code][response.short_code.to_sym][:weight]
         end
+        counts[response.short_code] += 1
       end
     end
 
-    scores
+    # Provide average for each question answered
+    scores.map{|code, value| [code, value / counts[code]] }.to_h
   end
 end
