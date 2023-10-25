03d51898eb67acfd95e9a1347d2bd3eb11ae1d67
Henry Balen <henry.balen@gmail.com>
2023-10-25 12:52:54 -0400

    Create separate start survey button component

---
+++ app/frontend/components/surveys/survey_start_button.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-button variant="success" 
+      :state="state"
+      @click="onNext"
+      data-cy="start-survey-button"
+      :disabled="disabled"
+    >{{ $t('next') }}</b-button>
+  </div>
+</template>
+
+<script>
+import modelMixin from '@/mixins/model.mixin';
+import modelUtilsMixin from '@/mixins/model_utils.mixin'
+import Tr from "@/i18n/translation"
+
+export default {
+  name: "SurveyStartButton",
+  props: {
+    access_code: null,
+    respondent_id: null,
+    state: null,
+    disabled: false
+  },
+  mixins: [
+    modelMixin,
+    modelUtilsMixin
+  ],
+  methods: {
+    onNext: function () {
+      this.$router.push(`/${Tr.getPersistedLocale()}/survey/submit/${this.access_code}`);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
